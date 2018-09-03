7c49f5b67746e712b83fc3a23a67016cf0294668
kot <kotsmilewtm@gmail.com>
2018-09-04 00:37:36 +0300

    need some tunning

--- PongPlay.py
+++ PongPlay.py
@@ -18,22 +18,32 @@ def play():
         while game.running:
 
             action1 = PongGame.ACTION_NONE
-            action2 = 0
+            action2 = PongGame.ACTION_NONE
+
+            if pygame.key.get_pressed()[K_w]:
+                action1 = PongGame.ACTION_UP
+            if pygame.key.get_pressed()[K_s]:
+                action1 = PongGame.ACTION_DOWN
+            if pygame.key.get_pressed()[K_UP]:
+                action2 = PongGame.ACTION_UP
+            if pygame.key.get_pressed()[K_DOWN]:
+                action2 = PongGame.ACTION_DOWN
+
             for event in pygame.event.get():
 
                 if event.type == QUIT:
                     pygame.quit()
                     sys.exit()
 
-                elif event.type == KEYDOWN:
-                    if event.key == K_w:
-                        action1 = PongGame.ACTION_UP
-                    if event.key == K_s:
-                        action1 = PongGame.ACTION_DOWN
-                    if event.key == K_UP:
-                        action2 = PongGame.ACTION_UP
-                    if event.key == K_DOWN:
-                        action2 = PongGame.ACTION_DOWN
+            # elif event.type == KEYDOWN:
+            #     if event.key == K_w:
+            #         action1 = PongGame.ACTION_UP
+            #     elif event.key == K_s:
+            #         action1 = PongGame.ACTION_DOWN
+            #     if event.key == K_UP:
+            #         action2 = PongGame.ACTION_UP
+            #     elif event.key == K_DOWN:
+            #         action2 = PongGame.ACTION_DOWN
 
             game.next(action1, action2)
 
